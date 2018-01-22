67d2f59b1b598ae0ce732f4a8a4a4f390c75a2b3
BealBC <Beal.BC@gmail.com>
2018-01-22 11:36:19 -0800

    Second Commit

--- CommunityAssistw18/Controllers/HomeController.cs
+++ CommunityAssistw18/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CommunityAssistw18.Models;
 
 namespace CommunityAssistw18.Controllers
 {
@@ -10,7 +11,9 @@ namespace CommunityAssistw18.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+            return View(db.BusinessRules.ToList());
         }
 
         public ActionResult About()
