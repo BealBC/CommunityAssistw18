501c832b5f253de677922af0cde08e4c6e13e608
BealBC <Beal.BC@gmail.com>
2018-02-07 12:28:41 -0800

    Register Assignment

---
+++ CommunityAssistw18/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistw18.Models;
+
+namespace CommunityAssistw18.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "FirstName, Email, Phone, PlainPassword, Apartment, Street, City, State, Zipcode")]NewPerson p)
+        {
+            int result = db.usp_Register(p.LastName, p.FirstName, p.Email, p.Phone, p.PlainPassword, p.Apartment, p.Street, p.City, p.State, p.Zipcode);
+
+            if (result != -1)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CommunityAssistw18/Models/NewPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssistw18.Models
+{
+    public class NewPerson
+    {
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public string PlainPassword { set; get; }
+        public string Apartment { set; get; }
+        public string Street { set; get; }
+        public string City { set; get; }
+        public string State { set; get; }
+        public string Zipcode { set; get; }
+    }
+}
