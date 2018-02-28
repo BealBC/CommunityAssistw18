328a2a3b7588c9e6f8ae90c205562eb79b882112
BealBC <beal.bc@gmail.com>
2018-02-28 11:00:36 -0800

    Add Model
    New Grant Model

---
+++ CommunityAssistw18/Models/NewGrant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssistw18.Models
+{
+    public class NewGrant
+    {
+        public string Type { set; get; }
+        public decimal Amount { set; get; }
+        public string Reason { set; get; }
+    }
+}
