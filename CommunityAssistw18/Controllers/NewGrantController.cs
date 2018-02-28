b34e8bb7ffa62ebd41791dbfd2b46a90ae6c6786
BealBC <beal.bc@gmail.com>
2018-02-28 10:59:05 -0800

    Add Controller
    NewGrantController

---
+++ CommunityAssistw18/Controllers/NewGrantController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistw18.Models;
+
+namespace CommunityAssistw18.Controllers
+{
+    public class NewGrantController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        // GET: NewGrant
+        public ActionResult Index()
+        {
+            if (Session["reviewerKey"] == null)
+            {
+                MessageClass m = new MessageClass();
+                m.MessageText = "You must be logged in to add a review.";
+                return RedirectToAction("Result", m);
+            }
+            ViewBag.GrantType = new SelectList(db.GrantTypes, "GrantTypeKey, GrantTypeName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Type, Reason, Amount")] NewGrant ng)
+        {
+            GrantApplication g = new GrantApplication();
+            
+            g.GrantApplicationReason = ng.Reason;
+            g.GrantApplicationRequestAmount = ng.Amount;
+            g.GrantAppicationDate = DateTime.Now;
+
+            db.GrantApplications.Add(g);
+            db.SaveChanges();
+
+            MessageClass m = new MessageClass();
+            m.MessageText = "Thank you, the book has been added";
+
+            return View("Result", m);
+
+        }
+
+        public ActionResult Result(MessageClass m)
+        {
+            return View(m);
+        }
+    }
+}
